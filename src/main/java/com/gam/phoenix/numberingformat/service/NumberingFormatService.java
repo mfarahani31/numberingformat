ed5a7505abbb51193a6a57401bbaa2de80e67efa
farahani <farahani@gamelectronics.com>
2020-03-11 11:32:48 +0330

    NumberingFormatService.java added

---
+++ src/main/java/com/gam/phoenix/numberingformat/service/NumberingFormatService.java
@@ -0,0 +1,28 @@
+package com.gam.phoenix.numberingformat.service;
+
+import com.gam.phoenix.numberingformat.model.NumberFormat;
+import com.gam.phoenix.numberingformat.repository.NumberingFormatRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class NumberingFormatService {
+
+    private NumberingFormatRepository numberingFormatRepository;
+
+    @Autowired
+    public NumberingFormatService(NumberingFormatRepository numberingFormatRepository) {
+        this.numberingFormatRepository = numberingFormatRepository;
+    }
+
+    public List<NumberFormat> findAllNumberFormat() {
+        return numberingFormatRepository.findAll();
+    }
+
+    public Optional<NumberFormat> findById(Long id) {
+        return this.numberingFormatRepository.findById(id);
+    }
+}

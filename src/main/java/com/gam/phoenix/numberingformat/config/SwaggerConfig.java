a8a5a0ba4fcd260d2dd44ebccf2db6dda8047d68
Mohammad Mahdavi <m-mahdavi@gamelectronics.com>
2020-04-12 11:42:29 +0430

    - swagger config updated

--- src/main/java/com/gam/phoenix/numberingformat/config/SwaggerConfig.java
+++ src/main/java/com/gam/phoenix/numberingformat/config/SwaggerConfig.java
@@ -1,32 +1,50 @@
 package com.gam.phoenix.numberingformat.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
-
+public class SwaggerConfig implements WebMvcConfigurer {
     @Bean
-    public Docket numberingFormatApi() {
-
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors
-                        .basePackage("com.gam.phoenix.numberingformat"))
-                .paths(PathSelectors.any())
-                .build().apiInfo(apiEndPointsInfo());
+    public Docket apiDocket() {
+        return (new Docket(DocumentationType.SWAGGER_2))
+                .globalOperationParameters(
+                        Arrays.asList(
+                                new ParameterBuilder()
+                                        .name("USERNAME")
+                                        .modelRef(new ModelRef("string"))
+                                        .parameterType("header")
+                                        .required(false)
+                                        .build()))
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.regex("(?!/error).+"))
+                .paths(PathSelectors.regex("(?!/actuator).+"))
+                .build()
+                .apiInfo(apiInfo());
     }
 
-    private ApiInfo apiEndPointsInfo() {
 
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    private ApiInfo apiInfo() {
         return new ApiInfoBuilder().title("Numbering-Format Service")
                 .description("Numbering-Format Service")
                 .license("Apache 2.0")

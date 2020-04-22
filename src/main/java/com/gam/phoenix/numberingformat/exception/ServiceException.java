11a94885218f862e3fbd70053a40358f240ba323
farahani <farahani@gamelectronics.com>
2020-04-22 15:51:39 +0430

    - ServiceException; remove unused Annotation
    Signed-off-by: farahani <farahani@gamelectronics.com>

--- src/main/java/com/gam/phoenix/numberingformat/exception/ServiceException.java
+++ src/main/java/com/gam/phoenix/numberingformat/exception/ServiceException.java
@@ -1,15 +1,19 @@
 package com.gam.phoenix.numberingformat.exception;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 
 /**
  * @author Mohammad Farahani (farahani@gamelectronics.com)
  **/
 
-@EqualsAndHashCode(callSuper = true)
-@Data
+@Setter
+@Getter
+@NoArgsConstructor
+@ToString
 public class ServiceException extends Exception {
     private static final long serialVersionUID = 5270039361418646509L;
     private final Object[] args = new Object[0];

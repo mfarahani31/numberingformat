821d5ad0c84c220904a3a75cfb869276ca9ec9f3
farahani <farahani@gamelectronics.com>
2020-04-27 12:39:18 +0430

    - refactor exception messages
    Signed-off-by: farahani <farahani@gamelectronics.com>

--- src/main/java/com/gam/phoenix/numberingformat/constants/ExceptionMessage.java
+++ src/main/java/com/gam/phoenix/numberingformat/constants/ExceptionMessage.java
@@ -6,11 +6,11 @@ package com.gam.phoenix.numberingformat.constants;
  **/
 
 public class ExceptionMessage {
-    public static final String NUMBERING_FORMAT_NOT_FOUND_EXCEPTION = "NUMBERINGFORMAT-S-NFS-001";
+    public static final String NUMBERING_FORMAT_NOT_FOUND_EXCEPTION = "NBR-S-NFS-001";
     public static final String NUMBERING_FORMAT_NOT_FOUND_EXCEPTION_MSG = "NumberingFormat does not exists";
 
 
-    public static final String NUMBERING_FORMAT_DUPLICATION_EXCEPTION = "NUMBERINGFORMAT_S_NFS_002";
+    public static final String NUMBERING_FORMAT_DUPLICATION_EXCEPTION = "NBR_S_NFS_002";
     public static final String NUMBERING_FORMAT_DUPLICATION_EXCEPTION_MSG = "Duplicate NumberingFormat !!!";
 
 
